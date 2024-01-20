e2a99f09a5e43f60e88c600fdac5b28aa67f2553
xRuuuuiiii <155722219+xRuuuuiiii@users.noreply.github.com>
2024-01-20 23:30:27 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    titleWeb: "Jen",
+    introTitle: 'Jen',
+    introDesc: `Thinking of you like crazy. 
+    Wish I could be cuddled up next to you right now, 
+    watching the sunset. Missing your smile and that way you laugh 
+    when I tell silly jokes.
+    Can't wait to see you again!`,
+    btnIntro: '^^HiHi^^',
+    title: 'I have fallen in love? 🥰',
+    desc: 'Maybe I was just smitten the moment I saw your smile. It made my heart skip a beat. Do you feel the same way?',
+    btnYes: 'I want to be with you!<33',
+    btnNo: 'No :3',
+    question: 'Theres something special about you, and I cant imagine not being around you more. Would you be my girlfriend?<3',
+    btnReply: 'I have a gift for you. <3',
+    reply: 'If you love me, you will tell me. Love if you love, dont love if you dont love.',
+    mess: 'I know you like me too!😘😘',
+    messDesc: 'Pick you up to go out soon, okay?',
+    btnAccept: 'Lets do it!<3',
+    messLink: 'https://www.instagram.com/renzznj/'
+}
